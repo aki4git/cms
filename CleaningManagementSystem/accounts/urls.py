e33c937b72c5ced8ffccd6ece11dde2cc0b5021b
akanno <excellion0315@gmail.com>
2016-08-26 10:59:52 +0900

    login_page move in app:accounts

--- CleaningManagementSystem/accounts/urls.py
+++ CleaningManagementSystem/accounts/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from accounts import views
 
 urlpatterns = [
+    url(r'^login/$', auth_views.login, {'template_name': 'accounts/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout,{'template_name': 'accounts/logged_out.html'}, name='logout'),
     url(r'^profile/$', views.profile, name='profile'),
 ]
